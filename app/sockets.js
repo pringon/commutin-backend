e5ca668ee51827edfc35a166642499b1f4a6adde
Daniel Goje <gojedan98@yahoo.com>
2018-06-10 15:59:26 +0300

    Random commit

--- app/sockets.js
+++ app/sockets.js
@@ -6,12 +6,7 @@ module.exports = (io) => {
 
     socket.on("set id", id => {
       socket.id = id;
-    })
-
-    socket.on("send data", data => {
-      console.log(`User with id ${socket.id} send message ${data}`);
-      let reversedData = data.split('').reverse().join('');
-      socket.emit("response", reversedData);
+      console.log(`Client with id ${id} has connected.`);
     });
   });
 };
