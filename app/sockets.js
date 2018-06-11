6daf62457d919356dcb47cb1f677db78efaaf110
Daniel Goje <gojedan98@yahoo.com>
2018-06-11 13:50:58 +0300

    Create mock driver communication handler

--- app/sockets.js
+++ app/sockets.js
@@ -9,8 +9,13 @@ module.exports = (io) => {
       console.log(`Client with id ${id} has connected.`);
     });
 
-    socket.on("tracking run", runCount => {
-      console.log(`${runCount}`);
+    socket.on("location change", coordinates => {
+      coordinates = JSON.parse(coordinates);
+      console.log(`Client's latitude is ${coordinates[0]} and longitude is ${coordinates[1]}`);
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`Client with id ${socket.id} has disconnected`);
     });
   });
 };
